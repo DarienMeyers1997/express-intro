9da6e103d13ee981f7ccfae793b0e1fc981eec9e
dd-code-immersives <domilamad89@gmail.com>
2023-02-02 11:38:49 -0500

    fixed parens

--- app.js
+++ app.js
@@ -50,31 +50,33 @@ app.get("/single-movie/:titleToSend", (req, res) => {
     success: true,
     foundMovie: foundMovie,
   });
-  app.post("/new-movie/", (req, res) => {
-    favoriteMovieList.push(req);
-    res.json({
-      success: true,
-    });
-  });
-  app.put("/update-movie/:titleToSend", (req, res) => {
-    foundMovie = favoriteMovieList.find((movie) => {
-      return movie.title === req.params.titleToSend;
-    });
-    res.json({
-      success: true,
-      foundMovie: foundMovie,
-    });
-  });
-  app.delete("/delete-movie/:titleToSend", (req, res) => {
-    foundMovie = favoriteMovieList.find((movie) => {
-      return movie.title === req.params.titleToSend;
-    });
-    res.json({
-      success: true,
-      foundMovie: foundMovie,
-    });
+
+});
+app.post("/new-movie/", (req, res) => {
+  favoriteMovieList.push(req);
+  res.json({
+    success: true,
   });
 });
+app.put("/update-movie/:titleToSend", (req, res) => {
+  foundMovie = favoriteMovieList.find((movie) => {
+    return movie.title === req.params.titleToSend;
+  });
+  res.json({
+    success: true,
+    foundMovie: foundMovie,
+  });
+});
+app.delete("/delete-movie/:titleToSend", (req, res) => {
+  foundMovie = favoriteMovieList.find((movie) => {
+    return movie.title === req.params.titleToSend;
+  });
+  res.json({
+    success: true,
+    foundMovie: foundMovie,
+  });
+});
+
 // return foundMovie;
 //I couldn't figure out what to do for this one. I am severely lacking in my knowledge up to this point in class. I struggled pretty bad in WDI 100.
 //}
