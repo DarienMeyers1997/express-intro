7cfab9f99b2c60a66cac350be13b3ce1bf3f9ef9
DarienMeyers1997 <darienmeyers@codeimmersives.com>
2023-05-16 15:04:56 -0400

    added the port to app.js line 6-13

--- app.js
+++ app.js
@@ -1,84 +1,8 @@
-// Bring in Express Code
-const express = require("express");
-
-const bodyParser = require("body-parser");
-
+const express = require("express"); //enables us to use any modules we create
 const app = express();
-const port = 3001;
+const PORT = 3001;
 
-// The next two lines are called MIDDLEWARE and they modify the request before
-
-//parse application/x.www-form-urlencoded
-app.use(express.json());
-//parse application/json
-// app.use(bodyParser.json())
-
-const favoriteMovieList = [
-  {
-    title: "Star Wars",
-    starRating: 5,
-    isRecommeded: true,
-    createAt: new Date(),
-    lastModified: new Date(),
-  },
-  {
-    title: "The Avengers",
-    starRating: 4,
-    isRecommeded: true,
-    createAt: new Date(),
-    lastModified: new Date(),
-  },
-];
-console.log(favoriteMovieList);
-
-app.get("/all-movies/", (req, res) => {
-  res.send("Hello World!");
+// app.listen - Is listening for PORT = 3001
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-// for (let index = 0; index < favoriteMovieList.length; index++) {
-//   const foundMovie = [];
-
-app.get("/single-movie/:titleToSend", (req, res) => {
-  const foundMovie = favoriteMovieList.find((movie) => {
-    return movie.title === req.params.titleToSend;
-  });
-  res.json({
-    success: true,
-    foundMovie: foundMovie,
-  });
-
-});
-app.post("/new-movie/", (req, res) => {
-  favoriteMovieList.push(req);
-  res.json({
-    success: true,
-  });
-});
-app.put("/update-movie/:titleToSend", (req, res) => {
-  foundMovie = favoriteMovieList.find((movie) => {
-    return movie.title === req.params.titleToSend;
-  });
-  res.json({
-    success: true,
-    foundMovie: foundMovie,
-  });
-});
-app.delete("/delete-movie/:titleToSend", (req, res) => {
-  foundMovie = favoriteMovieList.find((movie) => {
-    return movie.title === req.params.titleToSend;
-  });
-  res.json({
-    success: true,
-    foundMovie: foundMovie,
-  });
-});
-
-// return foundMovie;
-//I couldn't figure out what to do for this one. I am severely lacking in my knowledge up to this point in class. I struggled pretty bad in WDI 100.
-//}
-
-//console.log(foundMovie);
--- intro_express.js
+++ intro_express.js
@@ -1,13 +1,1 @@
-const express = require("express"); //importing the express library
-const app = express(); //instantiating the express library
-const port = 3000; //defining the port to run our application on
 
-//we are defining the main rout of 127.0.0.1:3000/
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-//run the applicaion, we pass the port , so we know what application the port is running on
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
