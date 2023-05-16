8b3e09c53df4064efef6c48c9e54c58305acb45f
DarienMeyers1997 <darienmeyers@codeimmersives.com>
2023-05-16 16:11:00 -0400

    created movies.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+/*
+npm install nodemon - Installs nodemon library
+npm install --save-dev nodemon - Installs package-lock.json 
+npm install express - Installs package.json 
+*/
 const express = require("express"); //enables us to use any modules we create
+const movies = require("./movies");
 const app = express();
 const PORT = 3001;
 
+app.get("/", (req, res) => {
+  res.status(200).json({ DataTransferItem: movies });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("from about page");
+});
 // app.listen - Is listening for PORT = 3001
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
