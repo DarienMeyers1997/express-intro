9c51dbbc3691887e8a63e46a576b2738041cffe6
DarienMeyers1997 <darienmeyers@codeimmersives.com>
2023-05-17 12:47:25 -0400

    made two git routes for genre in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ npm install express - Installs package.json
 const express = require("express"); //enables us to use any modules we create
 const movies = require("./movies");
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.status(200).json({ DataTransferItem: movies });
@@ -15,6 +15,33 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.status(200).send("from about page");
 });
+
+app.get("/action", (req, res) => {
+  const actionFilms = movies.filter((elem) => {
+    //.includes
+    //.search
+    return elem.Genre.includes("Action");
+  });
+  // console.log(actionFilms);
+  res.status(200).json({ data: actionFilms });
+});
+
+app.get("/genre/:genreName", (req, res) => {
+  //http://localhost:3000/genre/action
+  console.log(req.params);
+  console.log(req.params.genreName);
+  const genre = req.params.genreName.toLowerCase();
+  const filterByGenre = movies.filter((film) => {
+    return film.Genre.toLowerCase().includes(genre);
+  });
+
+  res.status(200).json({ data: filterByGenre });
+});
+
+//Create = POST request
+//Update = PUT request
+//Delete = DELETE request
+
 // app.listen - Is listening for PORT = 3001
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
